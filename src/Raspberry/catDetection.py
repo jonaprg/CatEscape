09bc7aab62bf545a680f99184c87adf7f9f34e9f
kolibriconk <36661173+kolibriconk@users.noreply.github.com>
2023-06-04 13:10:09 +0200

    Update catDetection.py

--- src/Raspberry/catDetection.py
+++ src/Raspberry/catDetection.py
@@ -1,26 +1,39 @@
 import cv2
+import sys
+import smbus2 as smbus
+import time
+
+# Slave Addresses
+I2C_SLAVE_ADDRESS = 11
 
 # define a vi' deo capture object
-face_cascade = cv2.CascadeClassifier('haarscascade_frontalcatface_extended.xml')
-cap = cv2.VideoCaptura(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+cap = cv2.VideoCapture(0)
+
+def notify_cat_detected():
+    try:
+        I2Cbus = smbus.SMBus(1)
+        with smbus.SMBus(1) as I2Cbus:
+            I2Cbus.write_i2c_block_data(I2C_SLAVE_ADDRESS, 0x00,[1])
+            time.sleep(0.5)
+    except:
+        pass
 
 while(True):
-
     # Capture the video frame
     # by frame
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5, minSize=(75,75))
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h), (255,255,0), 2)
-        roi_gray=gray[y:y + h,x:x + w]
-        roi_color = img[y:y + h, x:x + w]
+        cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0), 2)
         print("Cat Detected")
+        notify_cat_detected()
+        
 
-    cv2.imshow('Cat Detector', frame)
+    # cv2.imshow('Cat Detector', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
